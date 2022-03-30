44751d9b4f466b70eaa406b8d39a1542410c6ff8
Ahmed Ali <ahmedawan43011@gmail.com>
2022-03-30 13:08:20 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './MyFirstProgram'
+import MyButton from './MyFirstProgram';
+function App() {
+  return (
+    <div className="App">
+     <h1>Welcome to my first Program</h1>
+     <MyButton/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MyFirstProgram.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+
+const user = {
+    name: 'Ahmed Ali',
+    imageUrl: 'https://avatars.githubusercontent.com/u/52359086?v=4',
+    imageSize: 90,
+  };
+  
+  function Profile() {
+    return (
+      <>
+        <h1>{user.name}</h1>
+        <img
+          className="avatar"
+          src={user.imageUrl}
+          alt={'Photo of ' + user.name}
+          style={{
+            width: user.imageSize,
+            height: user.imageSize
+          }}
+        />
+      </>
+    );
+  }
+  
+  export default Profile;
