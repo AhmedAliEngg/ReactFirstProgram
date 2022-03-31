80e7e7db83476d18f867acaa70beb26004a913cc
Ahmed Ali <ahmedawan43011@gmail.com>
2022-03-31 11:32:45 +0500

    updating-data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import Profile from './MyFirstProgram';
+// import Profile from './MyFirstProgram';
 // import YourCast from './ListItem';
-import RespondingEvent from './RespondingEvent';
+// import RespondingEvent from './RespondingEvent';
+import MyButton from './UpdatingEvent';
 function App() {
   return (
     <div className="App">
      <h1>Welcome to my first Program</h1>
-     <Profile/>
+     {/* <Profile/> */}
      {/* <YourCast/> */}
-     <RespondingEvent/>
+     {/* <RespondingEvent/> */}
+     <MyButton/>
     </div>
   );
 }
